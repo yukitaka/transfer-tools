7f194870165387a99c29bf5757c5170f8a01d43a
Sato Takayuki <info@come-on.tech>
2024-01-24 22:43:50 +0900

    feat: add base of confluence

---
+++ confluence/model/base.py
@@ -0,0 +1,44 @@
+import json
+import os
+import requests
+from requests.auth import HTTPBasicAuth
+
+class Base:
+    def __init__(self, directory):
+        self.dir = directory
+
+    @staticmethod
+    def get(path, query=None):
+        return Base.request('get', path, query)
+
+    @staticmethod
+    def post(path, query=None):
+        return Base.request('post', path, query)
+
+    @staticmethod
+    def request(method, path, query=None):
+        domain = os.environ['CONFLUENCE_DOMAIN']
+        user = os.environ['ATLASSIAN_USER']
+        token = os.environ['ATLASSIAN_TOKEN']
+        if query is None:
+            query = {}
+
+        url = f"https://{domain}{path}"
+        print(f"{method} {url}")
+        auth = HTTPBasicAuth(user, token)
+        headers = {
+            "Accept": "application/json"
+        }
+
+        res = requests.request(
+            method,
+            url,
+            headers=headers,
+            params=query,
+            auth=auth
+        )
+
+        if res.status_code != requests.codes.ok:
+            return False
+
+        return json.loads(res.text)
